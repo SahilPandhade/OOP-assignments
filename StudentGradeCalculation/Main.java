b966b4c943f82b01be7f78c16063a79d954cb8af
Sahil R Pandhade <sahil.21910635@viit.ac.in>
2021-05-21 09:21:50 +0530

    Update Main.java

--- StudentGradeCalculation/Main.java
+++ StudentGradeCalculation/Main.java
@@ -1,3 +1,10 @@
+/*WAP in java that calculates the grade of N students based on the marks entered by user in each subject. 
+Program prints the grade based on this logic.
+If the average of marks is >= 80 then prints Grade ‘A’
+If the average is <80 and >=60 then prints Grade ‘B’
+If the average is <60 and >=40 then prints Grade ‘C’
+else prints Grade ‘D’ */
+
 //Java program to calculate grade of student
 import java.util.Arrays;
 import java.util.Scanner;
